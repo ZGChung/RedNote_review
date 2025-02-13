0ccb3671874536e88367599654e8dcf6c5d60397
ZGChung <zgchung@outlook.com>
2025-02-13 23:26:54 +0800

    feat: update key

--- src/secrets.py
+++ src/secrets.py
@@ -1,3 +1,3 @@
-API_KEY = "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"
+API_KEY = "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"
 API_BASE_URL = "https://openrouter.ai/api/v1"
 LLM_MODEL = "deepseek/deepseek-r1-distill-llama-8b"
